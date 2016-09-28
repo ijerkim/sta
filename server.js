ac26943c5049b8f368e53eccf3d0531e3790dc8a
ijerkim <ijerkim@gmail.com>
2016-09-28 13:47:50 +0900

    gm6

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var http    = require("http");
 var express = require("express");
 
@@ -5,3 +6,22 @@ var httpApp = express();
 httpApp.use(express.static(__dirname));
 
 var webServer = http.createServer(httpApp).listen(8080);
+*/
+
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
